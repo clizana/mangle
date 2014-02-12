8319edf7cab251ea1899a2e6c4fc87f1558a4b8f
Cristian Lizana Rojas <clizana@users.noreply.github.com>
2014-02-12 15:17:12 -0300

    Update pdfimage.py
    Updating my email to a valid one.

--- mangle/pdfimage.py
+++ mangle/pdfimage.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Cristian Lizana <cristian@lizana.in>
+# Copyright (C) 2012  Cristian Lizana <cristianlizanarojas@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
